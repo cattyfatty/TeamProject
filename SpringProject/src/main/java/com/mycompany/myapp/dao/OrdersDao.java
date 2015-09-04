b40b328bb06b8a1d665e3e48d661b42feeb831ad
Jaeyoon Lee <ljaeyoon820@gmail.com>
2015-09-04 16:30:35 +0900

    ordersdao수

--- SpringProject/src/main/java/com/mycompany/myapp/dao/OrdersDao.java
+++ SpringProject/src/main/java/com/mycompany/myapp/dao/OrdersDao.java
@@ -22,117 +22,99 @@ public class OrdersDao {
 
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
-	
+
 	public Integer insert(Orders orders) throws SQLException {
 		Integer pk = null;
 		String sql = "insert into orders (order_price, member_id) values (?,?)";
-		
+
 		KeyHolder keyHolder = new GeneratedKeyHolder();
-		jdbcTemplate.update(new PreparedStatementCreator(){
-		@Override
-		public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
-		PreparedStatement pstmt = conn.prepareStatement(sql, new String[]{"order_no"});
-		pstmt.setInt(1, orders.getOrderPrice());
-		pstmt.setString(2, orders.getMemberid());
-		return pstmt;
-		}
-	}, keyHolder);
+		jdbcTemplate.update(new PreparedStatementCreator() {
+			@Override
+			public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
+				PreparedStatement pstmt = conn.prepareStatement(sql, new String[] { "order_no" });
+				pstmt.setInt(1, orders.getOrderPrice());
+				pstmt.setString(2, orders.getMemberid());
+				return pstmt;
+			}
+		}, keyHolder);
 		Number keyNumber = keyHolder.getKey();
-		//getKey()로 orderNo를 얻을 수 있음
+		// getKey()로 orderNo를 얻을 수 있음
 		pk = keyNumber.intValue();
 		return pk;
 
 	}
 
-
 	public int delete(int orderNo) {
-		String sql = 
-		"delete from orders where orders_no=?";
-		int rows=jdbcTemplate.update(
-				sql,
-				orderNo
-			);
+		String sql = "delete from orders where orders_no=?";
+		int rows = jdbcTemplate.update(sql, orderNo);
 		return rows;
 	}
-	
 
 	public List<Orders> selectByPage(int pageNo, int rowsPerPage) {
 
-		String sql="";
+		String sql = "";
 		sql += "select order_no, order_date, order_arrival, order_price, member_id ";
 		sql += " from ";
 		sql += " order by order_no desc ";
-		sql +="limit ?,?";
-	
+		sql += "limit ?,?";
 
-		List<Orders> list = jdbcTemplate.query(
-				sql, 
-				new Object[] { (pageNo-1)*rowsPerPage, rowsPerPage },
-				new RowMapper<Orders>(){
+		List<Orders> list = jdbcTemplate.query(sql, new Object[] { (pageNo - 1) * rowsPerPage, rowsPerPage },
+				new RowMapper<Orders>() {
 
-			@Override
-			public Orders mapRow(ResultSet rs, int rowNum) throws SQLException {
-			Orders orders= new Orders();
-			orders.setOrderNo(rs.getInt("order_no"));
-			orders.setOrderPrice(rs.getInt("order_price"));
-			orders.setMemberid(rs.getString("member_id"));
-			return orders;
+					@Override
+					public Orders mapRow(ResultSet rs, int rowNum) throws SQLException {
+						Orders orders = new Orders();
+						orders.setOrderNo(rs.getInt("order_no"));
+						orders.setOrderPrice(rs.getInt("order_price"));
+						orders.setMemberid(rs.getString("member_id"));
+						return orders;
+					}
+
 				}
 
-			}
-				
 		);
 		return list;
 
 	}
 
-
 	public Orders selectByPk(int orderNo) {
-		
+
 		String sql = "select * from orders where order_no=?";
-		Orders orders = jdbcTemplate.queryForObject(
-				sql,
-				new Object[] {orderNo},
-				new RowMapper<Orders>() {
+		Orders orders = jdbcTemplate.queryForObject(sql, new Object[] { orderNo }, new RowMapper<Orders>() {
 
 			@Override
 			public Orders mapRow(ResultSet rs, int rowNum) throws SQLException {
-			Orders orders = new Orders();
-			orders.setMemberid(rs.getString("member_id"));
-			orders.setOrderNo(rs.getInt("order_no"));
-			orders.setOrderPrice(rs.getInt("order_price"));
-			return orders;
+				Orders orders = new Orders();
+				orders.setMemberid(rs.getString("member_id"));
+				orders.setOrderNo(rs.getInt("order_no"));
+				orders.setOrderPrice(rs.getInt("order_price"));
+				return orders;
 			}
 		}
-				
-	);
-			return orders;
+
+		);
+		return orders;
 	}
 
+	public List<Orders> selectByMemberId(String memberId) {
 
-	public List<Orders> selectByMemberId(String memberId){
-		
 		String sql = "select * from orders where member_id=?";
-		
-		List<Orders> list = jdbcTemplate.query(
-				sql,
-				new Object[] {memberId},
-				new RowMapper<Orders>() {
+
+		List<Orders> list = jdbcTemplate.query(sql, new Object[] { memberId }, new RowMapper<Orders>() {
 
 			@Override
 			public Orders mapRow(ResultSet rs, int rowNum) throws SQLException {
-			Orders orders= new Orders();
-			orders.setOrderNo(rs.getInt("order_no"));
-			orders.setOrderPrice(rs.getInt("order_price"));
-			orders.setMemberid(rs.getString("member_id"));
-			return orders;
-				}
-		
+				Orders orders = new Orders();
+				orders.setOrderNo(rs.getInt("order_no"));
+				orders.setOrderPrice(rs.getInt("order_price"));
+				orders.setMemberid(rs.getString("member_id"));
+				return orders;
 			}
+
+		}
 
 		);
 		return list;
 	}
 
 }
-
